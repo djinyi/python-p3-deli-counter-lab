5d33a8535300361c9f468613b9c5784efac0a229
Daisy Yi <daisyyi@rocketmail.com>
2025-02-23 17:16:05 -0700

    problem 2 solved

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -11,8 +11,13 @@ def line(queu):
         print("The line is currently empty.")
         return "The line is currently empty."
     
-def take_a_number():
-    pass
+def take_a_number(queu, new_person):
+    if len(queu) > 0:
+        queu.append(new_person)
+        print(f"Welcome, {new_person}. You are number {queu.index(new_person) +1} in line.")
+    else:
+        queu.append(new_person)
+        print(f"Welcome, {new_person}. You are number {queu.index(new_person) +1} in line.")
     
 def now_serving():
     pass
